e7c9d10d3730a4ac88770553a07d1359d5df0cd0
twinh <twinhuang@qq.com>
2018-04-23 17:42:10 +0800

    菜单入口更新

--- src/Plugin.php
+++ src/Plugin.php
@@ -7,4 +7,14 @@ class Plugin extends \Miaoxing\Plugin\BasePlugin
     protected $name = '菜单分类';
 
     protected $description = '包含个性菜单和默认菜单';
+
+    public function onAdminNavGetNavs(&$navs, &$categories, &$subCategories)
+    {
+        $navs[] = [
+            'parentId' => 'wechat-account',
+            'url' => 'admin/wechat-menu-categories',
+            'name' => '菜单管理',
+            'sort' => 800,
+        ];
+    }
 }
