b5ca232abc89b6a48b65eadaa03b001f65d9abb8
twinh <twinhuang@qq.com>
2018-07-17 17:05:57 +0800

    chore: 菜单细节更新

--- src/Plugin.php
+++ src/Plugin.php
@@ -8,6 +8,8 @@ class Plugin extends \Miaoxing\Plugin\BasePlugin
 
     protected $description = '包含个性菜单和默认菜单';
 
+    protected $adminNavId = 'wechat';
+
     public function onAdminNavGetNavs(&$navs, &$categories, &$subCategories)
     {
         $navs[] = [
